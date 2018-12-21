af895ce85c5edd6ec81e701afa8421f2b9a62176
Robert Novotny <novotnr0@gmail.com>
2018-12-21 12:00:58 +0100

    Refactor label to improve compatibility with IDE versions

--- src/com/github/novotnyr/idea/consul/ui/UnsynchronizedChangesLabelAction.java
+++ src/com/github/novotnyr/idea/consul/ui/UnsynchronizedChangesLabelAction.java
@@ -1,17 +1,37 @@
 package com.github.novotnyr.idea.consul.ui;
 
-public class UnsynchronizedChangesLabelAction extends LabelAction {
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.actionSystem.ex.CustomComponentAction;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.util.ui.UIUtil;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+
+public class UnsynchronizedChangesLabelAction extends AnAction implements CustomComponentAction {
     public static final String UNSYNCHED_CHANGES_LABEL_TEXT = "Changes are not synced";
 
-    public UnsynchronizedChangesLabelAction() {
-        super("");
-    }
+    private JLabel label = new JBLabel("", UIUtil.ComponentStyle.SMALL);
 
     public void hideText() {
-        getLabel().setText("");
+        this.label.setText("");
     }
 
     public void showText() {
-        getLabel().setText(UNSYNCHED_CHANGES_LABEL_TEXT);
+        this.label.setText(UNSYNCHED_CHANGES_LABEL_TEXT);
+    }
+
+    @NotNull
+    @Override
+    public JComponent createCustomComponent(@NotNull Presentation presentation) {
+        return this.label;
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+        // do nothing
     }
 }
