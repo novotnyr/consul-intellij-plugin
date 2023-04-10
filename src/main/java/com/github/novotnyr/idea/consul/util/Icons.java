2f536752d1e3343d5751c8a355a0dbbb410072eb
Robert Novotny <novotnr0@gmail.com>
2023-04-10 12:52:27 +0200

    Remove reflection for platform-available methods

--- src/main/java/com/github/novotnyr/idea/consul/util/Icons.java
+++ src/main/java/com/github/novotnyr/idea/consul/util/Icons.java
@@ -1,31 +1,11 @@
 package com.github.novotnyr.idea.consul.util;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.util.IconLoader;
 
 import javax.swing.Icon;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public abstract class Icons {
     public static Icon export() {
-        Icon icon = IconLoader.getIcon("/actions/export.png");
-        //noinspection ConstantConditions
-        if (icon != null) {
-            return icon;
-        }
-        return load("/toolbarDecorator/export.svg");
+        return IconLoader.getIcon("/actions/export.png", Icons.class);
     }
-
-    // Supports IJ 2020.1+
-    private static Icon load(String path) {
-        try {
-            Class<?> iconManagerClass = Class.forName("com.intellij.ui.IconManager");
-            Method getInstanceMethod = iconManagerClass.getMethod("getInstance", String.class, Class.class);
-            return (Icon) getInstanceMethod.invoke(null, path, AllIcons.class);
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | ClassNotFoundException e) {
-            return null;
-        }
-    }
-
 }
