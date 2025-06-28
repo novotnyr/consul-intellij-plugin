7ad9959f0302d8a792a5081cc15814d15f99ae3e
Robert Novotny <novotnr0@gmail.com>
2025-06-28 23:35:50 +0200

    Migrate to IntelliJ Platform Gradle Plugin builds

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij.platform") version "2.6.0"
+}
+
+group = "com.github.novotnyr"
+version = "7-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    implementation("com.ecwid.consul:consul-api:1.4.5")
+    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("commons-codec:commons-codec:1.13")
+    implementation("org.apache.httpcomponents:httpclient:4.5.13")
+
+    testImplementation("junit:junit:4.13.2")
+
+    intellijPlatform {
+        intellijIdeaCommunity("2022.3.3")
+        pluginVerifier()
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "223"
+            untilBuild = "252.*"
+        }
+        changeNotes = """
+            <ul>
+            <li></li>
+            </ul>
+        """.trimIndent()
+    }
+    publishing {
+        val intellijPublishToken: String by project
+        token = intellijPublishToken
+    }
+    pluginVerification {
+        ides {
+            recommended()
+        }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
